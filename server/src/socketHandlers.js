465460a0642ee64453fd4a450347abc1f72a2dc7
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-15 13:41:57 +0400

    Add socketHandlers.js

--- server/src/socketHandlers.js
+++ server/src/socketHandlers.js
@@ -1,5 +1,8 @@
 function registerSocketHandlers(io){
   io.on('connection',socket=>{
+    socket.on('joinRoom',room=>{});
+    socket.on('leaveRoom',room=>{});
+    socket.on('chatMessage',data=>{});
   });
 }
 module.exports={registerSocketHandlers};
